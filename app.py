6eded0c4cec7b922b7ade6fd7a58b0d060c6d034
Konark Mittal <49273687+konarkm68@users.noreply.github.com>
2024-10-10 22:58:03 +0000

    Fri, Oct 11, 2024, 4:28 AM +05:30

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for, jsonify
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from icecream import ic
+#from icecream import ic
 
 from helpers import apology, login_required
 
